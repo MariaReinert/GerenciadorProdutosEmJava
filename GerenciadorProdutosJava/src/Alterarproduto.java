b513dfb63cbe219b729bd2a672cb62347916cbf5
MARIA HEROLLDINA IVETTE REINERT <maria.reinert2@alunos.sc.senac.br>
2025-04-16 19:47:33 -0300

    Alterar Produto

---
+++ GerenciadorProdutosJava/src/Alterarproduto.java
@@ -0,0 +1,68 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Alterarproduto {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> ids = new ArrayList<>();
+        ArrayList<String> nomes = new ArrayList<>();
+
+        int proximold = 1;
+
+        while (true) {
+            System.out.println("\n ##### MENU #####");
+            System.out.println("1 - Cadastrar Produto");
+            System.out.println("2 - Listar Produtos");
+            System.out.println("3 - Alterar Produto");
+            System.out.println("4 - Excluir Produto");
+            System.out.println("5 - Sair");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            if (opcao == 1) {
+                System.out.println(" Nome do produto: ");
+                String nome = scanner.nextLine();
+
+
+                ids.add(proximold++);
+                nomes.add(nome);
+                System.out.println("Produtp cadastrado com sucesso!");
+
+            } else if (opcao == 2) {
+                if (ids.isEmpty()) {
+                    System.out.println("Nenhum produto cadastrado");
+                } else {
+                    System.out.println("\n ## Lista de Produtos ##");
+                    for (int i = 0; i < ids.size(); i++) {
+                        System.out.println(" ID: " + ids.get(i) + " Nome: " + nomes.get(i));
+                    }
+                }
+            } else if (opcao == 3) {
+                System.out.println("Digite o ID do produto a alterar");
+                int idAlt = scanner.nextInt();
+                scanner.nextLine();
+
+                int index = ids.indexOf(idAlt);
+                if (index != -1) {
+                    System.out.println("Novo nome: ");
+                    nomes.set(index, scanner.nextLine());
+                    System.out.println("Produto alterado com sucesso!");
+                } else {
+                    System.out.println("Produto não encontrado");
+                }
+            }
+        }
+    }
+}
+
+
+
+
